c9a67d1b1072a393f4086b80b19bfc5740d5ba27
Douglas <mercurymuur86@yahoo.com>
2023-01-25 15:07:48 -0500

    complete

---
+++ app.js
@@ -0,0 +1,34 @@
+var Doug = 36
+var Isaiah = 12
+var Nasir = 8
+var Hassan = 5
+var success = 2023
+var gains = 245
+var waterFast = 40
+var spiderMan = 1962
+var spawn = 1992
+var xMen = 1963
+
+    //Addition
+console.log(Doug + Isaiah);
+console.log(Nasir + Hassan);
+
+    //Subtraction
+console.log(Hassan - Nasir);
+console.log(success - spiderMan);
+
+    //Multiplication
+console.log(success * gains);
+console.log(spawn * spiderMan)
+
+    //Division
+console.log(xMen / waterFast)
+console.log(spawn / success)
+
+    //Increment
+console.log(++success)
+console.log(++Hassan)
+
+    //Decrement
+console.log(--gains)
+console.log(--waterFast)
